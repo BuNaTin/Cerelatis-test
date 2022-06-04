fec7aaaba10aadf7bc6486aa8390f0bf7641f24b
daniil <dddmitriev@hex.team>
2022-06-04 15:38:21 +0300

    hotfix

--- main.cpp
+++ main.cpp
@@ -1,6 +1,3 @@
-#include <Hello.h>
+#include "Net/Server.h"
 
-int main(int argc, char* argv[]) {
-    Cerelatis::Server::sayHello();
-    return 0;
-}
+int main(int argc, char *argv[]) { return 0; }
